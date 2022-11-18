bc06400fe506e5cb7ee91ddc7997409739201ea8
Aleksander Gregorka <alekzs@users.noreply.github.com>
2022-11-18 19:40:23 +0100

    title edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 
-			alert("it works");
+			alert("it worksnt");
 			const scene = new THREE.Scene();
 			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
